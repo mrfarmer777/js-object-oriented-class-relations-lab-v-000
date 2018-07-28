aa85475d5a05b3e0a30df9f2a026b73e9d04339d
Matt Farmer <mrfarmer777@gmail.com>
2018-07-28 05:39:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let drv_id=0;
 store={drivers:[], trips:[], passengers:[]};
+
 class Driver{
   constructor(name){
     this.id=++drv_id;
@@ -12,6 +13,24 @@ class Driver{
       let drv_trips=[];
       if(trip.driver_id==this.id){
         drv_trips.push(trip);
+      }
+    });
+  }
+}
+
+let pass_id=0;
+class Passenger{
+  constructor(name){
+    this.id=++pass_id;
+    this.name=name;
+    store.passengers.push(this);
+  }
+  
+  trips(){
+    store.trips.filter(function(trip){
+      let pass_trips=[];
+      if(trip.pass_id==this.id){
+        pass_trips.push(trip);
       }
     });
   }
